21e6cc764c7e366c4dad5ab292914ba01e8ce3b0
Silas Boutwell <128196495+SilasBoutwell@users.noreply.github.com>
2025-05-06 11:58:06 -0700

    Added the initial form and started designing too

---
+++ GameStore/frmGameStore.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace GameStore
+{
+    partial class frmGameStore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rchGameInventory = new System.Windows.Forms.RichTextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rchGameInventory
+            // 
+            this.rchGameInventory.Location = new System.Drawing.Point(40, 46);
+            this.rchGameInventory.Name = "rchGameInventory";
+            this.rchGameInventory.Size = new System.Drawing.Size(444, 281);
+            this.rchGameInventory.TabIndex = 0;
+            this.rchGameInventory.Text = "";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(601, 300);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(83, 27);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(601, 267);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(83, 27);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add Game";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // frmGameStore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 391);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.rchGameInventory);
+            this.Name = "frmGameStore";
+            this.Text = "48 BIts Game Store";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rchGameInventory;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
